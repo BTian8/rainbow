317e284ffd9bdd5f7afe5f44888ee3934e297153
BTian8 <boyan.t@wustl.edu>
2022-08-23 23:35:27 -0500

    twitch

--- src/main/java/com/example/rainbow/service/RegisterService.java
+++ src/main/java/com/example/rainbow/service/RegisterService.java
@@ -2,6 +2,7 @@ package com.example.rainbow.service;
 
 import com.example.rainbow.dao.RegisterDao;
 import com.example.rainbow.entity.db.User;
+import com.example.rainbow.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ public class RegisterService {
     private RegisterDao registerDao;
 
     public boolean register(User user) throws IOException {
+        user.setPassword(Util.encryptPassword(user.getUserId(), user.getPassword()));
         return registerDao.register(user);
     }
 }
